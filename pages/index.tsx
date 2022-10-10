19a5fdf3c7f8ab980abb69a10289b478f8f6e9b6
Yan Gordeev <ygordeev@griddynamics.com>
2022-10-10 22:37:52 +0200

    Dependencies installed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
+// Roboto font
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 const Home: NextPage = () => {
   return (
     <div>
